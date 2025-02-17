e4171b32b4c5204f44fa1929d7f91879fdcda2fe
Hee4885 <hijeon80@gmail.com>
2025-02-17 22:42:06 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.52 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] array) {
         Arrays.sort(array);
-        int indexNum = (array.length+1)/2-1;
+        //중앙값 구하는 공식 : {(자료의 개수)+1}/2 이지만, -1,+1이라는 계산식이 들어가므로 두 식은 제외
+        int indexNum = array.length/2;
         int answer = array[indexNum];
         return answer;
     }
