772450148a36e9632b5628c3b056feb9dfc5f5d5
Hee4885 <hijeon80@gmail.com>
2025-02-18 22:28:07 +0900

    [level 0] Title: 공배수, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        int answer = 0;
-        
-        if(number%n==0 && number%m==0){
-            answer = 1;
-        }
-        
+        int answer = (number%n==0 && number%m==0)? 1: 0;
         return answer;
     }
 }
