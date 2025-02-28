bf2e3f8dd6ad9af684ec0504a3b3f59645689753
Hee4885 <hijeon80@gmail.com>
2025-02-28 12:00:00 +0900

    고양이 문제 코드 수정

--- 백준/Bronze/10171. 고양이/고양이.java
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String [] args){
+	//고양이를 출력하는 코드
         System.out.println("\\    /\\");
         System.out.println(" )  ( ')");
         System.out.println("(  /  )");
