968da454b7a95a97045e0cdbd24c3414df5b33f1
전희진 <hijeon80@gmail.com>
2025-08-18 18:40:10 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,13 +1,3 @@
 def solution(hp):
-    count = 0
-    while hp > 0 :
-        if hp>=5 :
-            hp -= 5
-            count += 1
-        elif hp >= 3 :
-            hp -= 3
-            count += 1
-        else :
-            hp -= 1
-            count += 1
-    return count
+    answer = (hp//5)+((hp%5)//3)+((hp%5)%3)
+    return answer
