992f3f71fd9582de49da8f9ac7ad46c98ecb56f4
전희진 <hijeon80@gmail.com>
2025-08-19 08:34:17 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.02 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.py
@@ -0,0 +1,15 @@
+def solution(balls, share):
+    n = 1
+    m = 1
+    n_m = 1
+    
+    for i in range(1,balls+1) :
+        n *= i
+        
+    for j in range(1,(balls-share)+1) :
+        n_m *= j
+        
+    for k in range(1,share+1) :
+        m *= k
+        
+    return n/(n_m*m) 
