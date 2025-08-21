1fe22a50d6d329f6af8a462b616f69ae4aee391e
전희진 <hijeon80@gmail.com>
2025-08-22 08:53:47 +0900

    [level 0] Title: 합성수 찾기, Time: 0.24 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.py
@@ -9,6 +9,7 @@ def solution(n):
         count = 0
     for k in arr[::-1] :
         if k < 3 :
-            arr.remove(k)
+            arr.remove(k) 
+# remove는 앞에서 부터 요소를 삭제하면 인덱스가 삭제된 인덱스 이후부터 실행되므로 한 인덱스씩 밀린다.
     return len(arr)
 
