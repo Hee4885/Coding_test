e48dcfc3a029db572ff141a9b6d9602e78627471
전희진 <hijeon80@gmail.com>
2025-12-22 08:29:09 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.62 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.js
@@ -1,6 +1,9 @@
 function solution(arr, queries) {
-    for(let i = 0; i<queries.length; i++){
-        [arr[queries[i][0]], arr[queries[i][1]]] = [arr[queries[i][1]], arr[queries[i][0]]];  
-    }
+    // for(let i = 0; i<queries.length; i++){
+    //     [arr[queries[i][0]], arr[queries[i][1]]] = [arr[queries[i][1]], arr[queries[i][0]]];  
+    // }
+    
+    queries.forEach(([i,j])=>{[arr[i],arr[j]]= [arr[j],arr[i]]});
+    
     return arr;
 }
