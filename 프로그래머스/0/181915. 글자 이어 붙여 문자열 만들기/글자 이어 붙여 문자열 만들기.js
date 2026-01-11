58423baece81dfedba007392e8861f7716f9443f
전희진 <hijeon80@gmail.com>
2026-01-11 22:25:50 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -1,7 +1,9 @@
 function solution(my_string, index_list) {
-    var answer = '';
-    for(let n of index_list){
-        answer += my_string[n];
-    }
+    // var answer = '';
+    // for(let n of index_list){
+    //     answer += my_string[n];
+    // }
+    
+    var answer = index_list.map((n)=>(my_string[n])).join('');
     return answer;
 }
