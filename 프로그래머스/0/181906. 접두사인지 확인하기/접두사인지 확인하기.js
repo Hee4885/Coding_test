a4f8ed4112cd70e7f0598acf5a59918d40cf56f9
전희진 <hijeon80@gmail.com>
2026-01-11 22:20:35 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,4 +1,10 @@
 function solution(my_string, is_prefix) {
     var answer = my_string.startsWith(is_prefix)? 1 : 0;
     return answer;
+    
+    /*
+    [ 앞에 +가 무슨 표시인거지? ] 
+        - boolean 앞에 + 해주면 true는 1, false는 0으로 변환해줍니다.
+        - 문자열 string 앞에 +를 붙여서 +string 해주면 데이터 타입이 number로 바뀌어요
+    */
 }
